1f503418b9e8e971d7b2feb5c618bfded00f3434
NITSAN Technologies <sanjay@nitsan.in>
2022-02-28 11:46:07 +0530

    [BUGFIX] Fix tooltip design issue for TYPO3 v10

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF['ns_license'] = array (
   'author_email' => 'sanjay@nitsan.in',
   'author_company' => 'NITSAN Technologies Pvt Ltd',
   'state' => 'stable',
-  'version' => '1.6.0',
+  'version' => '1.7.0',
   'constraints' => 
     array (
       'depends' => 
