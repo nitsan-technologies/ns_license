61ff4c99fbeab0c4829eddcbd19f01be1bab0793
Sanjay Chauhan <sanjay@nitsan.in>
2023-01-03 15:43:01 +0530

    [TASK] Improved TYPO3 backend login hook experiance

--- Classes/Controller/NsLicenseModuleController.php
+++ Classes/Controller/NsLicenseModuleController.php
@@ -751,7 +751,12 @@ class NsLicenseModuleController extends ActionController
             return json_decode($rawResponse);
         } catch (\Throwable $e) {
             $this->addFlashMessage($e->getMessage(), 'Your server has an issue connecting with our license system; Please get in touch with your server administrator with the below error message.', \TYPO3\CMS\Core\Messaging\AbstractMessage::ERROR);
-            //$this->redirect('list');
+            
+            // Let's only redirect if we are at TYPO3 backend module (ignore at Login)
+            $params = $this->request->getArguments();
+            if(isset($params['action'])) {
+                $this->redirect('list');
+            }
         }
     }
 
@@ -781,7 +786,12 @@ class NsLicenseModuleController extends ActionController
             $this->getBackupToUploadFolder($extKey);
         } catch (\Throwable $e) {
             $this->addFlashMessage($e->getMessage(), 'Your server has an issue connecting with our license system; Please get in touch with your server administrator with the below error message.', \TYPO3\CMS\Core\Messaging\AbstractMessage::ERROR);
-            //$this->redirect('list');
+            
+            // Let's only redirect if we are at TYPO3 backend module (ignore at Login)
+            $params = $this->request->getArguments();
+            if(isset($params['action'])) {
+                $this->redirect('list');
+            }
         }
     }
 
