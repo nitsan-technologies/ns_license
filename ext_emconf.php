5a7c1a0131e2bb747f7ef9fe7999dc02c3211919
NITSAN <sanjay@nitsan.in>
2024-05-04 13:12:05 +0530

    [BUGFIX] Domain deactivation issue fixed

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF['ns_license'] = array(
   'author_email' => 'sanjay@nitsan.in',
   'author_company' => 'NITSAN Technologies Pvt Ltd',
   'state' => 'stable',
-  'version' => '12.0.8',
+  'version' => '12.0.9',
   'constraints' =>
     array(
       'depends' =>
