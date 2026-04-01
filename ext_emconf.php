26924441b51e414b360677feb01c4fb5e192df76
Rushabh Makwana <rusabh.nitsan@gmail.com>
2026-04-01 17:29:12 +0530

    [TASK] Update TYPO3 dependencies and version; add German translation for Live Chat

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,35 +1,39 @@
 <?php
 
-$EM_CONF['ns_license'] = [
-    'title' => 'License Manager',
-    'description' => 'Manage licenses of your TYPO3 templates and extensions purchased from T3Planet. Includes activation and license validation. Documentation available at https://docs.t3planet.com/en/latest/License/LicenseActivation/Index.html',
-    'category' => 'templates',
-    'author' => 'Team T3Planet',
-    'author_email' => 'info@t3planet.de',
-    'author_company' => 'T3Planet',
-    'state' => 'stable',
-    'version' => '14.2.1',
-    'constraints'
-      => [
-          'depends'
-            => [
-                'typo3' => '12.0.0-14.9.99',
-                'extensionmanager' => '12.0.0-14.9.99',
-            ],
-          'conflicts'
-            => [
-            ],
-          'suggests'
-            => [
-            ],
-      ],
-    'autoload'
-      => [
-          'classmap'
-          => [
-              0 => 'Classes/',
-          ],
-      ],
-    'uploadfolder' => false,
-    'clearcacheonload' => false,
-];
+/***************************************************************
+ * Extension Manager/Repository config file for ext "ns_license".
+ *
+ * Auto generated 31-03-2026 16:31
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'License Manager',
+  'description' => 'Manage licenses of your TYPO3 templates and extensions purchased from T3Planet. Includes activation and license validation. Documentation available at https://docs.t3planet.com/en/latest/License/LicenseActivation/Index.html',
+  'category' => 'templates',
+  'version' => '14.2.2',
+  'state' => 'stable',
+  'uploadfolder' => false,
+  'clearcacheonload' => false,
+  'author' => 'Team T3Planet',
+  'author_email' => 'info@t3planet.de',
+  'author_company' => 'T3Planet',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '12.0.0-14.1.1',
+      'extensionmanager' => '12.0.0-14.1.1',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+);
+
