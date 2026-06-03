3f06ce6fcfb0cf5f5bb1035b6787b74f3dc02ea7
Divya <divya@nitsantech.com>
2026-06-03 18:55:40 +0530

    [TASK] Modify Version

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF['ns_license'] = [
     'author_email' => 'info@t3planet.de',
     'author_company' => 'T3Planet',
     'state' => 'stable',
-    'version' => '14.2.3',
+    'version' => '14.2.4',
     'constraints'
       => [
         'depends'
