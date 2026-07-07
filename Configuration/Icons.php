58d01dac947274480381a2f49bd8cbe4e43c2051
nitsan-technologies <sanjay@nitsantech.com>
2026-07-07 20:15:09 +0530

    [TASK] Adjust Icon as per v14

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -3,11 +3,16 @@
 declare(strict_types=1);
 
 use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+use TYPO3\CMS\Core\Information\Typo3Version;
+
+$isV14OrHigher = (new Typo3Version())->getMajorVersion() >= 14;
 
 return [
     'submodule-nslicense' => [
         'provider' => SvgIconProvider::class,
-        'source' => 'EXT:ns_license/Resources/Public/Icons/submodule-nslicense.svg',
+        'source' => $isV14OrHigher
+            ? 'EXT:ns_license/Resources/Public/Icons/submodule-nslicense-v14.svg'
+            : 'EXT:ns_license/Resources/Public/Icons/submodule-nslicense-v12.svg',
     ],
     'module-nitsan' => [
         'provider' => SvgIconProvider::class,
